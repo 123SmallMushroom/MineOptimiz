3c54a628f92dd6a379fd72a185b42a1937824376
123SmallMushroom <114296445+123SmallMushroom@users.noreply.github.com>
2023-05-01 17:35:57 +0800

    Update Build-Lite-Modpack.bat

--- Build-Lite-Modpack.bat
+++ Build-Lite-Modpack.bat
@@ -9,5 +9,6 @@ cmd rmdir /s fancymenu
 cmd cd ..
 cd /d ..
 cd ..
+cd ..
 powershell Compress-Archive -Path Tiao-s-Modpack-For-Tiao-s-Server -DestinationPath modpack.zip
 cmd ren modpack.zip modpack.mrpack
