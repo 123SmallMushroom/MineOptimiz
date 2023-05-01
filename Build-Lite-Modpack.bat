d1c02d3245cd8ea4df2791c24b2282497c09324b
123SmallMushroom <114296445+123SmallMushroom@users.noreply.github.com>
2023-05-01 16:50:05 +0800

    Update and rename Start-Build-Lite-Modpack.bat to Build-Lite-Modpack.bat

---
+++ Build-Lite-Modpack.bat
@@ -0,0 +1,13 @@
+cmd rmdir /s .github
+cmd rmdir /s .git
+cmd del README.md
+cmd cd /d overrides
+cmd del server.dat
+cmd del wallhaven-e7651w_1600x1024.png
+cmd cd /d config
+cmd rmdir /s fancymenu
+cmd cd ..
+cd /d ..
+cd ..
+powershell Compress-Archive -Path Tiao-s-Modpack-For-Tiao-s-Server -DestinationPath modpack.zip
+cmd ren modpack.zip modpack.mrpack
